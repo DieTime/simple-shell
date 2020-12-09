6b93297a635e1297ea9f5e9ec97285f338325236
Denis Glazkov <glazzk.off@mail.ru>
2020-12-09 13:06:11 +0300

    Add background tasks execution

--- shell.c
+++ shell.c
@@ -2,30 +2,27 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include <signal.h>
 #include <pwd.h>
 #include <wait.h>
 
 #include "shell.h"
 
-pid_t pid;
+tasks t = { .foreground = -1, .background = NULL, .cursor = 0, .capacity = 0 };
 
 void display() {
     // Try get and print username with color
-    // \033[92m - green color
-    // \033[0m  - reset color
     uid_t uid = geteuid();
     struct passwd *pw = getpwuid(uid);
     if (pw != NULL) {
-        printf("\033[92m%s\033[0m:", pw->pw_name);
+        printf("%s%s%s:", PRIMARY_COLOR, pw->pw_name, RESET_COLOR);
     }
 
     // Try get and print current directory with color
-    // \033[90m - grey color
-    // \033[0m  - reset color
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("\033[90m%s\033[0m", cwd);
+    char cwd[MAX_DIRECTORY_PATH];
+    if (getcwd(cwd, MAX_DIRECTORY_PATH) != NULL) {
+        printf("%s%s%s", SECONDARY_COLOR, cwd, RESET_COLOR);
     }
 
     // Print end of shell info
@@ -40,7 +37,7 @@ char* readline() {
 
     // Allocating memory for line
     char* buffer  = (char*)malloc(sizeof(char) * buff_size);
-    if (!buffer) {
+    if (buffer == NULL) {
         printf("[ERROR] Couldn't allocate buffer for reading!\n");
         return NULL;
     }
@@ -55,8 +52,8 @@ char* readline() {
         if (position >= buff_size) {
             buff_size *= 2;
 
-            buffer = realloc(buffer, buff_size);
-            if (!buffer) {
+            buffer = (char*)realloc(buffer, buff_size);
+            if (buffer == NULL) {
                 printf("[ERROR] Couldn't reallocate buffer for reading!\n");
                 return NULL;
             }
@@ -79,8 +76,8 @@ char** split(char* line) {
     char* token;
 
     // Allocate memory for tokens array
-    char** tokens = malloc(sizeof(char*) * buff_size);
-    if (!tokens) {
+    char** tokens = (char**)malloc(sizeof(char*) * buff_size);
+    if (tokens == NULL) {
         printf("[ERROR] Couldn't allocate buffer for splitting!\n");
         return NULL;
     }
@@ -95,8 +92,8 @@ char** split(char* line) {
         if (position >= buff_size) {
             buff_size *= 2;
 
-            tokens = realloc(tokens, buff_size * sizeof(char*));
-            if (!tokens) {
+            tokens = (char**)realloc(tokens, buff_size * sizeof(char*));
+            if (tokens == NULL) {
                 printf("[ERROR] Couldn't reallocate buffer for tokens!\n");
                 return NULL;
             }
@@ -121,6 +118,8 @@ int execute(char** args) {
         return help();
     } else if (strcmp(args[0], "quit") == 0) {
         return quit();
+    } else if (strcmp(args[0], "bg") == 0) {
+        return bg();
     } else {
        return launch(args);
     }
@@ -144,6 +143,7 @@ int help() {
         "Built in functions:\n"
         "  cd   \n"
         "  help \n"
+        "  bg   \n"
         "  quit \n\n"
 
         "Use the man command for information on other programs.\n"
@@ -153,12 +153,35 @@ int help() {
 }
 
 int quit() {
+    kill_all();
     return EXIT;
 }
 
+int bg() {
+    // Temp background task variable
+    bg_task* bt;
+
+    for (size_t i = 0; i < t.cursor; i++) {
+        // Store task in temp variable
+        bt = &t.background[i];
+
+        // Print info about task
+        printf(
+            "[%zu]%s cmd: %s%s;%s pid: %s%d; %s"
+            "state: %s%s;%s timestamp: %s%s", i,
+            SECONDARY_COLOR, RESET_COLOR, bt->cmd,
+            SECONDARY_COLOR, RESET_COLOR, bt->pid,
+            SECONDARY_COLOR, RESET_COLOR, bt->finished ? "finished" : "active",
+            SECONDARY_COLOR, RESET_COLOR, bt->timestamp
+        );
+    }
+
+    return CONTINUE;
+}
+
 int launch(char** args) {
-    int background; // Is background task
-    int status;
+    pid_t pid;        // Fork process id
+    int   background; // Is background task
 
     // Checking if task is background
     background = is_background(args);
@@ -176,20 +199,44 @@ int launch(char** args) {
         if (execvp(args[0], args) == -1) {
             printf("[ERROR] Couldn't execute unknown command!\n");
         }
+        exit(1);
     }
     // Parent process
     else {
         if (background) {
-            // TODO: Write background implementation
-            kill_foreground();
+            // Try add background task to array
+            if (add_background(pid, args[0]) == -1) {
+                // Kill all processes and free
+                // memory before exit
+                kill_all();
+
+                return EXIT;
+            }
+
+            // Add signal for removing background task
+            // from array on task finished
+            signal(SIGCHLD, mark_background);
         } else {
+            // Set foreground task as pid
+            set_foreground(pid);
+
             // Add signal for killing child on ctrl-c
             signal(SIGINT, kill_foreground);
 
-            // Wait child process end
-            do {
-                waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+            // Handle probably error
+            if (waitpid(pid, NULL, WUNTRACED) == -1) {
+                // Print problem
+                printf("[ERROR] Couldn't track child process!\n");
+
+                // Kill all processes and free
+                // memory before exit
+                kill_all();
+
+                return EXIT;
+            }
+
+            // Set foreground to default value
+            t.foreground = -1;
 
             // Reset signal
             signal(SIGINT, SIG_IGN);
@@ -221,7 +268,104 @@ int is_background(char** args) {
     return 0;
 }
 
+void set_foreground(pid_t pid) {
+    t.foreground = pid;
+}
+
 void kill_foreground() {
-    kill(pid, SIGTERM);
+    // Kill process
+    kill(t.foreground, SIGTERM);
+
+    // Set pid to default value
+    t.foreground = -1;
+
     printf("\n");
+}
+
+int add_background(pid_t pid, char* name) {
+    // Temp background task variable
+    bg_task* bt;
+
+    // If end of free space in background array - increase size
+    if (t.cursor >= t.capacity) {
+        t.capacity = t.capacity * 2 + 1;
+        t.background = (bg_task*)realloc(t.background, sizeof(bg_task) * t.capacity);
+        if (t.background == NULL) {
+            printf("[ERROR] Couldn't reallocate buffer for background tasks!\n");
+            return -1;
+        }
+    }
+
+    // Print info about process start
+    printf("[%zu] started.\n", t.cursor);
+
+    // Save task in temp variable
+    bt = &t.background[t.cursor];
+
+    // Save process info in array
+    bt->pid = pid;
+    bt->finished = 0;
+
+    time_t timestamp = time(NULL);
+    bt->timestamp = ctime(&timestamp);
+
+    bt->cmd = strdup(name);
+
+    // Move cursor right
+    t.cursor += 1;
+
+    return 0;
+}
+
+void mark_background() {
+    // Temp background task variable
+    bg_task* bt;
+
+    // Get process id of ended process
+    pid_t pid = wait(NULL);
+
+    // Handle error if background process doesn't tracking
+    if (pid == -1 && t.foreground == -1) {
+        printf("[WARNING] Couldn't get id of ended process!\n");
+        return;
+    }
+
+    // Search and remove process form background tasks array
+    for (size_t i = 0; i < t.cursor; i++) {
+        // Place task to temp variable
+        bt = &t.background[i];
+
+        if (bt->pid == pid) {
+            // Print info about process end
+            printf("[%zu] finished.\n", i);
+
+            // Set new state for background process
+            bt->finished = 1;
+
+            break;
+        }
+    }
+}
+
+void kill_all() {
+    // Temp background task variable
+    bg_task* bt;
+
+    // Kill foreground process
+    if (t.foreground != -1) {
+        kill_foreground();
+    }
+
+    for (size_t i = 0; i < t.cursor; i++) {
+        // Place task to temp variable
+        bt = &t.background[i];
+
+        // Kill process if active
+        if (!bt->finished) {
+            kill(bt->pid, SIGTERM);
+        }
+
+        // Free memory for command name
+        free(bt->cmd);
+    }
 }
--- shell.h
+++ shell.h
@@ -1,10 +1,33 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define DEFAULT_BUFF_SIZE 16
-#define TOKENS_DELIMITERS " \t"
+#define DEFAULT_BUFF_SIZE  16
+#define MAX_DIRECTORY_PATH 1024
+#define TOKENS_DELIMITERS  " \t"
 #define CONTINUE 1
 #define EXIT 0
+
+#define PRIMARY_COLOR   "\033[92m"
+#define SECONDARY_COLOR "\033[90m"
+#define RESET_COLOR     "\033[0m"
+
+// Struct of background task
+struct bg_task_t {
+    pid_t   pid;           // Process id
+    int     finished;      // Process state
+    char*   timestamp;     // Process state
+    char*   cmd;          // Command cmd
+};
+typedef struct bg_task_t bg_task;
+
+// Struct of all tasks
+struct tasks_t {
+    pid_t    foreground; // Process id of foreground task
+    bg_task* background; // Background tasks
+    size_t   cursor;     // Cursor of background tasks
+    size_t   capacity;   // Background array capacity
+};
+typedef struct tasks_t tasks;
 
 /**
  * Display left shell info like:
@@ -62,6 +85,13 @@ int help();
 int quit();
 
 /**
+ * Print background tasks
+ *
+ * @return status for main loop - CONTINUE
+ */
+int bg();
+
+/**
  * Executing linux shell commands
  *
  * @param args - tokens for executing
@@ -79,6 +109,39 @@ int launch(char** args);
  */
 int is_background(char** args);
 
+/**
+ * Save foreground process id
+ *
+ * @param pid - child foreground process id
+ */
+void set_foreground(pid_t pid);
+
+/**
+ * Ctrl-C handler for killing child
+ * foreground process
+ */
 void kill_foreground();
+
+/**
+ * Emplace background child process id
+ * into background tasks
+ *
+ * @param pid - child background process id
+ *
+ * @return 0 on success, -1 on error
+ */
+int add_background(pid_t pid, char* name);
+
+/**
+ * Child process end handler for
+ * removing task from background
+ */
+void mark_background();
+
+/**
+ * Killing all active processes
+ * before EXIT execute status
+ */
+void kill_all();
 
 #endif //SHELL_H
